2486c8949ee685fbd179808c82071e6f25758e35
pravesh <pravesh247@hotmail.com>
2024-03-30 15:17:08 +0400

    Begining of project Personal Portfolio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Greeting from './profilePage';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <Greeting />
+  );
+}
+
+export default App;
